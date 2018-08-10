3b5e54bf4a13c9f44bc3360773f270ec787b17df
M SAIFUL ISLAM <saifulbdjoy@gmail.com>
2018-08-10 06:45:04 +0600

    return multiple values using structures

--- ReturnMultipleValuesFromAMethod/ReturnMultipleValuesFromAMethod/Program.cs
+++ ReturnMultipleValuesFromAMethod/ReturnMultipleValuesFromAMethod/Program.cs
@@ -8,7 +8,7 @@ namespace ReturnMultipleValuesFromAMethod
 {
     public class Program
     {
-        //Method for returning multiple values using OUT keyword
+        #region "Method for returning multiple values using OUT keyword"
         static int DividedResultUsingOutKeyword(int numberOne, int numberTwo, out int remainder)
         {
             int quotient = 0;
@@ -25,8 +25,9 @@ namespace ReturnMultipleValuesFromAMethod
 
             return quotient;
         }
+        #endregion
 
-        //Method for returning multiple values using REF keyword
+        #region "Method for returning multiple values using REF keyword"
         static void DividedResultUsingRefKeyword(int numberOne, int numberTwo, ref int quotient, ref int remainder)
         {
             if (numberOne >= numberTwo)
@@ -40,18 +41,51 @@ namespace ReturnMultipleValuesFromAMethod
                 quotient = numberTwo / numberOne;
             }
         }
+        #endregion
+
+        #region "Declared a struct & a Method for returning multiple values using structures"
+        //Structure declaration
+        struct DividedInfo
+        {
+            public int Quotient { get; set; }
+            public int Remainder { get; set; }
+        }
+
+        //Method for returning multiple values using Structures
+        static DividedInfo DividedResultUsingStructures(int numberOne, int numberTwo)
+        {
+            DividedInfo dInfo = new DividedInfo();
+
+            if (numberOne >= numberTwo)
+            {
+                dInfo.Remainder = numberOne % numberTwo;
+                dInfo.Quotient = numberOne / numberTwo;
+            }
+            else
+            {
+                dInfo.Remainder = numberTwo % numberOne;
+                dInfo.Quotient = numberTwo / numberOne;
+            }
+
+            return dInfo;
+        }
+        #endregion
 
         static void Main(string[] args)
         {
-            //Calling method & get result by usign OUT keyword;
+            ////Calling method & get result by usign OUT keyword;
             //int remainder = 0;
             //int quotient = DividedResultUsingOutKeyword(7, 2, out remainder);
             //Console.WriteLine("The Quotient Value Is: {0} \nThe Remainder Value Is: {1}", quotient, remainder);
 
-            //Calling method & get result by usign REF keyword;
-            int quotient = 0, remainder = 0;
-            DividedResultUsingRefKeyword(7, 2, ref quotient, ref remainder);
-            Console.WriteLine("The Quotient Value Is: {0} \nThe Remainder Value Is: {1}", quotient, remainder);
+            ////Calling method & get result by usign REF keyword;
+            //int quotient = 0, remainder = 0;
+            //DividedResultUsingRefKeyword(7, 2, ref quotient, ref remainder);
+            //Console.WriteLine("The Quotient Value Is: {0} \nThe Remainder Value Is: {1}", quotient, remainder);
+
+            //Calling method & get result by usign Structures;
+            DividedInfo dInfo = DividedResultUsingStructures(7, 2);
+            Console.WriteLine("The Quotient Value Is: {0} \nThe Remainder Value Is: {1}", dInfo.Quotient, dInfo.Remainder);
 
             Console.ReadKey();
         }
